fd3ff2aef72852af4dd38ad13fdd03a7e7130a1a
Sander van Zutphen <sander@kjsoftware.nl>
2022-10-04 13:18:29 +0200

    Rate toegevoegd

--- src/UpdateConnector/FiEntries.php
+++ src/UpdateConnector/FiEntries.php
@@ -81,6 +81,9 @@ class FiEntries extends UpdateObject
                 ],
                 'Fref' => [
                     'alias' => 'Factuurreferentie',
+                ],
+                'Rate' => [
+                    'alias' => 'Factuurreferentie',
                 ]
             ],
         ];
